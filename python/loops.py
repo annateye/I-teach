4f75448cfd498696bd592f0a54854aba2b58e9cc
Anna <annateye4609@gmail.com>
2020-07-03 10:54:36 +0100

    to make changes

---
+++ python/loops.py
@@ -0,0 +1,37 @@
+number = 1
+
+while number <=20:
+    print("Number:" ,number)
+    number = number + 1
+
+
+
+
+
+
+for number in range(15, 30, 3):
+    print("Number: ", number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    names = [ "David", "Lartey", "John","Peter" ]
+    for name in names:
+
+       ("Name is: ", name)
+
+
+("Our loop is done!")
+
+
+
